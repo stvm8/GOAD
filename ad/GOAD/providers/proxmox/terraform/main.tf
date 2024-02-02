9e44556207aaf0868599056a6c5b3a0d3be4efdc
Takashi <hoc4life8@gmail.com>
2024-02-02 09:09:30 -0600

    fix GOAD

--- ad/GOAD/providers/proxmox/terraform/main.tf
+++ ad/GOAD/providers/proxmox/terraform/main.tf
@@ -15,7 +15,6 @@ provider "proxmox" {
     pm_tls_insecure = true
     pm_parallel = 3
     pm_timeout = 2400
-
     pm_log_enable = true
     pm_log_file = "terraform-plugin-proxmox.log"
     pm_log_levels = {
